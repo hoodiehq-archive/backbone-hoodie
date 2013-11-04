633eb94925ee4f3431a2b639f4fe8aa0e312541e
Sven Lito <me@svenlito.com>
2013-11-04 15:36:41 +0100

    remove obsolete dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-simple-mocha');
 
   grunt.registerTask('default', ['jshint', 'uglify']);
 
